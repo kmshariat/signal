59251238af268be32a8ffd1cf3bae87ced5ca9ee
K.M Shariat Ullah <cast.shariat@gmail.com>
2022-03-08 18:01:08 +0600

    aligned the triangle's apex

--- signal_sketch/triangular.py
+++ signal_sketch/triangular.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 time = np.linspace(0, 1, 1000)
-current = signal.sawtooth(2*np.pi*2*time) #This signal.squre command creates square signal from sinusoidal signal
+current = signal.sawtooth(2*np.pi*2*time,0.5) #This signal.squre command creates square signal from sinusoidal signal
 
 plt.title("Triangular Signal") 
 plt.xlabel("Time") #xlabel is used for labeling x axis
